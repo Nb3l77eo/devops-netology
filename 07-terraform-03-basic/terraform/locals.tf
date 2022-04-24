60b073e05616b531bb9502f9a0421df89e01ba80
root <root@vagrant.vm>
2022-04-24 21:50:44 +0000

    new file:   locals.tf         new file:   main.tf         new file:   meta.txt         new file:   network.tf         new file:   version.tf

---
+++ 07-terraform-03-basic/terraform/locals.tf
@@ -0,0 +1,19 @@
+locals {
+ node1_conf_instance_cores = {
+   stage = 2
+   prod = 4
+ }
+ node1_conf_instance_memory = {
+   stage = 2
+   prod = 4
+ }
+ node1_conf_instance_platfrom = {
+   stage = "standard-v3"
+   prod = "standard-v1"
+ }
+ node1_instance_count = {
+   stage = 1
+   prod = 2
+ }
+
+}
